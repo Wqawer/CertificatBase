5a44e87cf4bf1fda0fca28ac208deb3c547d8cc7
Michal Wawer <m.wqawer@gmail.com>
2019-11-25 19:48:14 +0100

    git

--- index.php
+++ index.php
@@ -1,31 +1,81 @@
+<html>
+    <body>
+        <form method="post" enctype='multipart/form-data'>
+            <input type="text" name="Name">
+            <input type="text" name="Surname">
+            <input type="tel" name="tel">
+            <input type="file" name="upfile" id="file" multiple>
+            <input type="submit">
+        </form>
+    </body>
+</html>
 <?php
-    define("MAX_SIZE", 5*1024*1024);
-    $extensions= ["jpeg","jpg","png"];
-?>
-<body>
-<form method="post" enctype='multipart/form-data'>
-    <input type="text" name="Name">
-    <input type="text" name="Surname">
-    <input type="tel" name="tel">
-    <input type="file" name="file[]" id="file" multiple>
-    <input type="submit">
-</form>
-<?php
-    if ($_SERVER['REQUEST_METHOD'] == 'POST' ) {
-        $count = count($_FILES['file']['name']);
-        if($count==2){
-            for($i=0;$i<$count;$i++){
-                if($_FILES['file']['size'][$i]<MAX_SIZE){
-                    
-                }
-                else{
-                    echo "<span style='color:red;'>Plik nr ".($i+1)." jest za duzy</span>";
-                }
-            }
-        }
-        else{
-            echo "<span style='color:red;'>Musisz dodać 2 zdjęcia</span>";
-        }
+//header('Content-Type: text/plain; charset=utf-8');
+
+try {
+   
+    // Undefined | Multiple Files | $_FILES Corruption Attack
+    // If this request falls under any of them, treat it invalid.
+    if (
+        !isset($_FILES['upfile']['error']) ||
+        is_array($_FILES['upfile']['error'])
+    ) {
+        throw new RuntimeException('Invalid parameters.');
     }
-    ?>
-</body>
+
+    // Check $_FILES['upfile']['error'] value.
+    switch ($_FILES['upfile']['error']) {
+        case UPLOAD_ERR_OK:
+            break;
+        case UPLOAD_ERR_NO_FILE:
+            throw new RuntimeException('No file sent.');
+        case UPLOAD_ERR_INI_SIZE:
+        case UPLOAD_ERR_FORM_SIZE:
+            throw new RuntimeException('Exceeded filesize limit.');
+        default:
+            throw new RuntimeException('Unknown errors.');
+    }
+
+    // You should also check filesize here.
+    if ($_FILES['upfile']['size'] > 1000000) {
+        throw new RuntimeException('Exceeded filesize limit.');
+    }
+
+    // DO NOT TRUST $_FILES['upfile']['mime'] VALUE !!
+    // Check MIME Type by yourself.
+    $finfo = new finfo(FILEINFO_MIME_TYPE);
+    if (false === $ext = array_search(
+        $finfo->file($_FILES['upfile']['tmp_name']),
+        array(
+            'jpeg' => 'image/jpeg',
+            'jpg' => 'image/jpg',
+            'png' => 'image/png',
+            'gif' => 'image/gif',
+        ),
+        true
+    )) {
+        throw new RuntimeException('Invalid file format.');
+    }
+
+    // You should name it uniquely.
+    // DO NOT USE $_FILES['upfile']['name'] WITHOUT ANY VALIDATION !!
+    // On this example, obtain safe unique name from its binary data.
+    if (!move_uploaded_file(
+        $_FILES['upfile']['tmp_name'],
+        sprintf('./uploads/test',
+            sha1_file($_FILES['upfile']['tmp_name']),
+            $ext
+        )
+    )) {
+        throw new RuntimeException('Failed to move uploaded file.');
+    }
+
+    echo 'File is uploaded successfully.';
+
+} catch (RuntimeException $e) {
+
+    echo $e->getMessage();
+
+}
+
+?>
